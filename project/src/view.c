679eca4593eab3240954475fdb2004c1271f6056
Ben Kirchman <b_kirchman@coloradocollege.edu>
2017-10-11 16:39:31 -0600

    fix

--- project/src/view.c
+++ project/src/view.c
@@ -14,11 +14,14 @@ float pixelToFloat(int pixel);
 void update();
 void move(int index);
 void look(int index);
+void addToAvg(long i);
+void printAvgToFile();
 double getDist(int i1, int i2);
 int flockSize = 2000;
-int windowSize = 1000;
+int windowSize = 2000;
 struct Node** flock;
 int pos = .5;
+FILE *fptr;
 
 void init()
 {
@@ -37,7 +40,12 @@ void drawTriangle(struct Node * node)
    	glEnd(); 
 }
 int delay = 10;
+struct timespec start_time;
+struct timespec end_time;
+long msec;
 void display() {
+	// Start timer.
+	clock_gettime(CLOCK_MONOTONIC, &start_time);
 	// Clear Color and Depth Buffers
 	//glutTimerFunc(delay, display, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -54,22 +62,30 @@ void display() {
 		move(i);
 		float x1 = pixelToFloat(flock[i]->x);
 		float y1 = pixelToFloat(flock[i]->y);
-		float x2 = pixelToFloat((flock[i]->x - flock[i]->xdir) - (flock[i]->ydir/2));
-		float y2 = pixelToFloat((flock[i]->y - flock[i]->ydir) + (flock[i]->xdir/2));
-		float x3 = pixelToFloat((flock[i]->x - flock[i]->xdir) + (flock[i]->ydir/2));
-		float y3 = pixelToFloat((flock[i]->y - flock[i]->ydir) - (flock[i]->xdir/2));
+		float x2 = pixelToFloat((flock[i]->x - flock[i]->xdir) - (flock[i]->ydir/2)*2);
+		float y2 = pixelToFloat((flock[i]->y - flock[i]->ydir) + (flock[i]->xdir/2)*2);
+		float x3 = pixelToFloat((flock[i]->x - flock[i]->xdir) + (flock[i]->ydir/2)*2);
+		float y3 = pixelToFloat((flock[i]->y - flock[i]->ydir) - (flock[i]->xdir/2)*2);
+		float color1 = ((float)((flock[i]->xdir + 10)*10)+400)/600;
+		float color2 = ((float)((flock[i]->ydir + 10)*10)+400)/600;
+		float color3 = 200;//((float)((flock[i]->xdir + flock[i]->ydir + 20)*5)+400)/600;
+		glColor3f(color1,color2,color3);
 		glVertex2f(x1,y1);
 		glVertex2f(x2,y2);
 		glVertex2f(x3,y3);
 	}
 	glEnd();
 	glutSwapBuffers();
+	// End timer.
+	clock_gettime(CLOCK_MONOTONIC, &end_time);
+	msec = (end_time.tv_sec - start_time.tv_sec)*1000 + (end_time.tv_nsec - start_time.tv_nsec)/1000000;
+	addToAvg(msec);
 }
 
 void getVisibleBoids(int subject, int* avgx, int* avgy, int* avgxdir, int* avgydir) {
 	int count = 0;
 	for(int current = 0; current < flockSize; current++) {
-		if(getDist(current,subject) < 100.0 && current != subject) {
+		if(getDist(current,subject) < 40.0 && current != subject) {
 			count++;
 			*avgx+=flock[current]->x;
 			*avgy+=flock[current]->y;
@@ -87,7 +103,7 @@ void getVisibleBoids(int subject, int* avgx, int* avgy, int* avgxdir, int* avgyd
 int getTooCloseBoids(int subject, int* tooCloseAvgX, int* tooCloseAvgY) {
 	int tooClose = 0;
 	for(int i = 0; i < flockSize; i++) {
-		if(getDist(subject,i) < 20.0 && i != subject) {
+		if(getDist(subject,i) < 30.0 && i != subject) {
 			tooClose++;
 			*tooCloseAvgX += flock[i]->x;
 			*tooCloseAvgY += flock[i]->y;
@@ -107,9 +123,9 @@ double getDist(int i1, int i2) {
 }
 
 int getRandV() {
-	int random =  (rand() %11) -5;
-	if(random != 0) {
-		return random;
+	int random =  (rand() %11);
+	if(random != 5) {
+		return random-5;
 	}
 	return getRandV();
 }
@@ -120,105 +136,107 @@ void look(int i) {
 	//go away from the center of mass of all too close boids OR
 	//allign direction to all nearby boids that are not too close 
 	//and move toward the center of all nearby boids;
+	
+	//xdir = xdir + xdirtoward + xdiraway + xavg + xbounddir
+	int xdirTo = 0;
+	int xdirAway = 0;
+	int xdirAvg = 0;
+	int xbounddir = 0;
+	int ydirTo = 0;
+	int ydirAway = 0;
+	int ydirAvg = 0;
+	int ybounddir = 0;
+	
 	int avgx = 0;
 	int avgy = 0;
-	int avgxdir = 0;
-	int avgydir = 0;	
-	getVisibleBoids(i, &avgx, &avgy, &avgxdir, &avgydir);
-	flock[i]->xdir = avgxdir;
-	flock[i]->ydir = avgydir;
+	getVisibleBoids(i, &avgx, &avgy, &xdirAvg, &ydirAvg);
+	int xdist = avgx - flock[i]->x;	
+	int ydist = avgy - flock[i]->y;
+	if(xdist !=0) {
+		xdirTo = xdist/abs(xdist);
+	}
+	if(ydist !=0) {
+		ydirTo = ydist/abs(ydist);	
+	}
 
-	int tooCloseAvgX = 0;
-	int tooCloseAvgY = 0;
-	int tooClose = getTooCloseBoids(i, &tooCloseAvgX, &tooCloseAvgY);
-	int tooCloseXdir = 0;
-	int tooCloseYdir = 0;
+	avgx = 0;
+	avgy = 0;
+	int tooClose = getTooCloseBoids(i, &avgx, &avgy);
 	if(tooClose > 0) {
-		//steer away from the avg location
-		int xdist = flock[i]->x - tooCloseAvgX;
-		int ydist = flock[i]->y - tooCloseAvgY;
-		int xavgdir = flock[i]->xdir;
-		int yavgdir = flock[i]->ydir;
-		if(abs(xdist) < abs(ydist)) {
-			if(ydist/3 != 0) {
-				yavgdir += 3 * (ydist/abs(ydist));
-				xavgdir += xdist/abs(ydist/3);
-			}
-		} else if(abs(xdist) > abs(ydist)) {
-			if(xdist/3 != 0) {
-				xavgdir += 3 * (xdist/abs(xdist));
-				yavgdir += ydist/abs(xdist/3);
-			}
+		xdist = flock[i]->x - avgx;
+		ydist = flock[i]->y - avgy;
+		if(xdist !=0) {
+			xdirAway = 2 * xdist/abs(xdist);
+			xdirTo = 0;
 		}
-		flock[i]->xdir = (xavgdir + flock[i]->xdir )/2;
-		flock[i]->ydir = (yavgdir + flock[i]->ydir)/2;;
-	} else { 	
-		int xdist = avgx - flock[i]->x;
-		int ydist = avgy - flock[i]->y;
-		int xavgdir = flock[i]->xdir;
-		int yavgdir = flock[i]->ydir;
-		if(abs(xdist) < abs(ydist)) {
-			if(ydist/5 != 0) {
-				xavgdir = xdist/abs(ydist/5);
-				yavgdir = 5 * (ydist/abs(ydist));
-			}
-		} else if(abs(xdist) > abs(ydist)) {
-			if(xdist/5 != 0) {
-				yavgdir = ydist/abs(xdist/5);
-				xavgdir = 5 * (xdist/abs(xdist));
-			}
+		if(ydist != 0) {
+			ydirAway = 2 * ydist/abs(ydist);
+			ydirTo = 0;
 		}
-		flock[i]->xdir = (flock[i]->xdir + xavgdir)/2;
-		flock[i]->ydir = (flock[i]->ydir + yavgdir)/2;
 	}
-	int y = flock[i]->y;
+		
+	// Edge detection
+	int ea = 1;
 	int x = flock[i]->x;
-	/*if(x < 500) {
-		if(y < 500) {
-			flock[i]->ydir +=1;
-		} else {
-			flock[i]->xdir +=1;	
-		}	
-	} else {
-		if(y < 500) {
-			flock[i]->xdir -=1;
-		} else {
-			flock[i]->ydir -=1;	
-		}
-	}*/
-	if(x < 50) {
-		flock[i]->xdir +=1;
-	} else if (x > 950) {
-	       flock[i]->xdir -=1;
+	int y = flock[i]->y;	
+	if(x < 100) {
+		xbounddir = ea;
+		//ybounddir = ea;
 	}
-       	if (y < 50) {
- 		flock[i]->ydir +=1;
-	} else if (y > 950) {
-		flock[i]->ydir-=1;
+	if (x > windowSize-100) {
+	       xbounddir = -ea;
+	       //ybounddir = -ea;
 	}
-	if(flock[i]->xdir == 0) {
-		flock[i]->xdir = getRandV();
+       	if (y < 100) {
+		//xbounddir = -ea;
+		ybounddir = ea;
+	}
+	if (y > windowSize-100) {
+		//xbounddir = ea;
+		ybounddir = -ea;
+	}
+
+	//accumulate
+	flock[i]->xdir +=  xdirTo + xdirAway + xdirAvg + xbounddir;
+	flock[i]->ydir +=  ydirTo + ydirAway + ydirAvg + ybounddir;
+	flock[i]->xdir /=2;	
+ 	flock[i]->ydir /=2;
+	
+	//cant stop wont stop
+	if(sqrt(flock[i]->xdir * flock[i]->xdir + flock[i]->ydir * flock[i]->ydir) < 6) {
+		flock[i]->xdir *=2;
+		flock[i]->ydir *=2;
 	}
 	
-	if(flock[i]->ydir == 0) {
-		flock[i]->ydir = getRandV();
+	int min = -10;
+	int max = 10;
+	flock[i]->xdir = fmax(min, fmin(max,flock[i]->xdir));
+	flock[i]->ydir = fmax(min, fmin(max,flock[i]->ydir));
+	max+=1;
+	//flip at edge
+	if(x < max) {
+		flock[i]->xdir = abs(flock[i]->xdir) + 1;
+	} 
+       	if (x > windowSize - max) {
+		flock[i]->xdir = (-1 * abs(flock[i]->xdir)) - 1;
 	}
-	int xdir = flock[i]->xdir;
-	if(xdir < 1 && xdir > -1) {
-		flock[i]->xdir = 1  * (xdir/abs(xdir));
+       	if (y < max) {
+		flock[i]->ydir = abs(flock[i]->ydir) + 1;
+	} 
+	if (y > windowSize - max) {
+		flock[i]->ydir = (-1 * abs(flock[i]->ydir)) - 1;
 	}
-	int ydir = flock[i]->ydir;
-	if(ydir < 1 && ydir > -1) {
-		flock[i]->ydir = 1 * (ydir/abs(ydir));
-	}
-	flock[i]->xdir = fmax(-5, fmin(5,flock[i]->xdir));
-	flock[i]->ydir = fmax(-5, fmin(5,flock[i]->ydir));
-}
+	
+}	
 
 void move(int i) {
 	flock[i]->x += flock[i]->xdir;
 	flock[i]->y += flock[i]->ydir;
-	if(flock[i]->x > windowSize) {
+	int min = 0;
+	int max = windowSize;
+	flock[i]->x = fmax(min, fmin(max,flock[i]->x));
+	flock[i]->y = fmax(min, fmin(max,flock[i]->y));
+	/*if(flock[i]->x > windowSize) {
 		flock[i]->x = 0;
 	}
 	if(flock[i]->x < 0) {
@@ -229,7 +247,7 @@ void move(int i) {
 	}
 	if(flock[i]->y < 0) {
 		flock[i]->y = windowSize;
-	}
+	}*/
 	glutPostRedisplay();
 }
 int count = 0;
@@ -277,16 +295,40 @@ void initFlock() {
 	srand((unsigned) time(&t));
 	for(int i = 0; i < flockSize; i++) {
 		flock[i] = malloc(sizeof(struct Node));
-		flock[i]->x = (rand() % 1000);
-		flock[i]->y = (rand() % 1000);
+		flock[i]->x = (rand() % windowSize);
+		flock[i]->y = (rand() % windowSize);
 		flock[i]->xdir = getRandV();
 		flock[i]->ydir = getRandV();
 	}
+}
+long avg[100];
+int counter=0;
+
+void addToAvg(long i) {
+	if(counter == 99) {
+		printAvgToFile();
+		counter = 0;
+	} else {
+		avg[counter] = i;
+		counter++;
+	}
+}
+
+void printAvgToFile() {
+	fptr = fopen("/home/b/Documents/parallel/project/src/avg.txt","a");
+	long averageToPrint = 0;
+	for(int i = 0; i < 99; i++) {
+		averageToPrint += avg[i];
+	}
+	averageToPrint/=99;
+	fprintf(fptr,"%d\n", averageToPrint);
+	fclose(fptr);
 }
 
 int main(int argc, char** argv) {
 	initFlock();
 	initViews(argc, argv);
 }
+
 
 
