31a62e0508524c92ab087700ff5fd31e4106fd76
Ben Kirchman <b_kirchman@coloradocollege.edu>
2017-10-15 13:42:42 -0600

    done

--- project/src/compile.sh
+++ project/src/compile.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-gcc  -g view.c -lm -lGL -lglut -o run
-./run
-
+gcc  -g serial.c -lm -lGL -lglut -o serial
+gcc -g -fopenmp openmp.c -lm -lGL -lglut -o openmp
+gcc -g -fopenmp display.c -lm -lGL -lglut -o display
+gcc -g -fcilkplus cilk.c -lm -lGL -lglut -o cilk
